0d5a52b69b151122ee3c05b8797af53dbea99509
Jace <jace.lant@gmail.com>
2023-08-17 23:34:09 -0600

    last

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 
-console.log(process.argv)
+
 
 /*data del koder 
 name
@@ -12,5 +12,26 @@ if (process.argv[2] === "add") {
         koders.push({ name: process.argv[3] })
         fs.writeFileSync("./koders.json", JSON.stringify(koders), "utf8")
     }
+}
+
+
+if (process.argv[2] === "ls") {
+    koders.forEach((koder) => {
+        console.log(koder.name)
+    })
+}
+
+if (process.argv[2] === "rm") {
+    if (process.argv[3] !== "") {
+        let newListKoder = koders.filter((koder) => {
+            return koder.name !== process.argv[3]
+        })
+        fs.writeFileSync("./koders.json", JSON.stringify(newListKoder), "utf8")
+    }
+}
+
+if (process.argv[2] === "reset") {
+    koders = []
+    fs.writeFileSync("./koders.json", JSON.stringify(koders), "utf8")
 }
 
